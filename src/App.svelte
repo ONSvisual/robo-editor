05a8fbb1441906ffd20fa4f9cb233dce41bf7480
Tim Kershaw <tim.kershaw@ons.gov.uk>
2022-09-30 09:10:28 +0100

    Changes to file loading and saving

--- src/App.svelte
+++ src/App.svelte
@@ -2,15 +2,15 @@
 	import { onMount } from "svelte";
 	import { csvParse, autoType } from "d3-dsv";
 	import { format } from "d3-format";
-	import parseColor from 'parse-color';
-	import { download, MagicArray, sleep } from "./utils";
+	import parseColor from "parse-color";
+	import { MagicArray, sleep } from "./utils";
 	import template_default from "./template";
 	import Editor from "./ui/Editor.svelte";
 	import Icon from "./ui/Icon.svelte";
-	
+
 	// DOM BINDINGS ETC
 	let rosae, editor, pug_upload, csv_upload;
-	
+
 	// DATA/STATE
 	let template = template_default;
 	let places = null;
@@ -18,29 +18,59 @@
 	let place = null;
 	let plaintext = false;
 	let progress = 0;
-	
+
 	function render(template, place, plaintext = false) {
 		try {
-			let str = rosae.render(template, {place, places, lookup, format, language: 'en_US'});
+			let str = rosae.render(template, {
+				place,
+				places,
+				lookup,
+				format,
+				language: "en_US",
+			});
 			// Fix to remove spaces added between numbers and prefix/suffix symbols by rosae
 			str = str.replace(/(?<=\d)\s+((?=%)|(?=p{2}))/g, "");
 			str = str.replace(/(?<=[£€\$])\s+(?=\d)/g, "");
 			// Fix to add spaces after closing </mark> </em> or <strong> tags unless followed by one of . , <
-			str = str.replace(/((?<=<\/mark>)|(?<=<\/strong>)|(?<=<\/em>)|(?<=<\/[abi]>))(?![\.,<:;])/g, " ");
+			str = str.replace(
+				/((?<=<\/mark>)|(?<=<\/strong>)|(?<=<\/em>)|(?<=<\/[abi]>))(?![\.,<:;])/g,
+				" "
+			);
 
 			if (!plaintext) {
 				// Hack in ID labels
 				let sections = str.match(/<section([^<]*?)>/g);
-				sections = Array.isArray(sections) ? sections.filter((d,i,arr) => arr.indexOf(d) == i) : [];
-				let ids = sections ? sections.map(s => s.match(/id="([^<]*?)"/)) : [];
-				let classes = sections ? sections.map(s => s.match(/class="([^<]*?)"/)) : [];
+				sections = Array.isArray(sections)
+					? sections.filter((d, i, arr) => arr.indexOf(d) == i)
+					: [];
+				let ids = sections
+					? sections.map((s) => s.match(/id="([^<]*?)"/))
+					: [];
+				let classes = sections
+					? sections.map((s) => s.match(/class="([^<]*?)"/))
+					: [];
 				sections.forEach((s, i) => {
-					str = str.replaceAll(s, `${s}${classes[i] ? `<span class="class-label">${classes[i][1]}</span>` : ''}${ids[i] ? `<span class="id-label">id: ${ids[i][1]}</span>` : ''}`);
+					str = str.replaceAll(
+						s,
+						`${s}${
+							classes[i]
+								? `<span class="class-label">${classes[i][1]}</span>`
+								: ""
+						}${
+							ids[i]
+								? `<span class="id-label">id: ${ids[i][1]}</span>`
+								: ""
+						}`
+					);
 				});
 				// Write in keys for custom props
 				let props = str.match(/<prop([^<]*?)>/g);
-				props = Array.isArray(props) ? props.filter((d,i,arr) => arr.indexOf(d) == i) : [];
-				let prop_classes = props ? props.map(s => s.match(/class="([^<]*?)"/)) : [];
+				props = Array.isArray(props)
+					? props.filter((d, i, arr) => arr.indexOf(d) == i)
+					: [];
+				let prop_classes = props
+					? props.map((s) => s.match(/class="([^<]*?)"/))
+					: [];
 				props.forEach((p, i) => {
 					str = str.replaceAll(p, `${p}${prop_classes[i][1]}: `);
 				});
@@ -48,12 +78,26 @@
 			// Contrasting text colours for highlighted <mark> texts
 			let marks = str.match(/<mark([^<]*?)>/g);
 			if (Array.isArray(marks)) {
-				marks = marks.filter((d,i,arr) => arr.indexOf(d) == i && d.includes("background-color"));
-				let colors = marks.map(d => d.match(/(?<=background-color:\s).+(?=[";])/)[0]);
-				colors.forEach(color => {
+				marks = marks.filter(
+					(d, i, arr) =>
+						arr.indexOf(d) == i && d.includes("background-color")
+				);
+				let colors = marks.map(
+					(d) => d.match(/(?<=background-color:\s).+(?=[";])/)[0]
+				);
+				colors.forEach((color) => {
 					let rgb = parseColor(color).rgb;
-					let text_color = (((rgb[0] * 299) + (rgb[1] * 587) + (rgb[2] * 114)) / 1000) > 125 ? "black" : "white";
-					str = str.replaceAll(`background-color: ${color}`, plaintext ? "" : `background-color: ${color}; color: ${text_color};`);
+					let text_color =
+						(rgb[0] * 299 + rgb[1] * 587 + rgb[2] * 114) / 1000 >
+						125
+							? "black"
+							: "white";
+					str = str.replaceAll(
+						`background-color: ${color}`,
+						plaintext
+							? ""
+							: `background-color: ${color}; color: ${text_color};`
+					);
 				});
 			}
 			return str;
@@ -62,70 +106,71 @@
 		}
 	}
 
-	function clickPUG() {
-		pug_upload.click();
-	}
-	function loadPUG() {
-		let file = pug_upload.files[0] ? pug_upload.files[0] : null;
-		
-		if (file) {
-			var reader = new FileReader();
-			reader.onload = function(e) {
-				editor.setContent(e.target.result);
-			};
-			reader.readAsText(file);
-		}
-	}
+	let fileHandle;
+	let currentPugName;
+	let currentCsvName;
+	let lastModifiedPug;
+	let lastModifiedCsv;
 
-	async function validatePUG() {
-		progress = 0;
-		let invalid = [];
+	async function openPUG() {
+		const options = {
+			types: [
+				{
+					description: "PUG file",
+					accept: {
+						"text/plain": [".pug"],
+					},
+				},
+			],
+		};
+		//	alert('clicked')
+		[fileHandle] = await window.showOpenFilePicker(options);
+		const file = await fileHandle.getFile();
+		const content = await file.text();
 
-		let plcs = places ? [null, ...places] : [null];
-
-		for (let i = 0; i < plcs.length; i ++) {
-			progress = i == plcs.length - 1 ? 0 : (i + 1) / plcs.length;
-			await sleep(0);
-			let output = render(template, plcs[i], plaintext);
-			if (output == "") invalid.push(plcs[i] ? plcs[i].areanm : "No area selected");
-		}
-
-		if (invalid[0]) {
-			alert(`Validation failed for ${invalid.join(', ')}`);
-		} else {
-			alert("Validation passed for all areas.");
-		}
+		currentPugName = fileHandle.name;
+		lastModifiedPug = file.lastModifiedDate.toGMTString();
+		console.log(lastModifiedPug);
+		editor.setContent(content);
 	}
 
-	function makeData(str) {
-		let newplaces = csvParse(str, autoType);
-		let newlookup = {};
-		newplaces.forEach(d => newlookup[d.areacd] = d);
+	async function openCSV() {
+		const options = {
+			types: [
+				{
+					description: "CSV file",
+					accept: {
+						"text/plain": [".csv"],
+					},
+				},
+			],
+		};
 
-		newplaces = newplaces.filter(d => ["E06","E07","E08","E09","W06"].includes(d.areacd.slice(0,3)))
-			.sort((a, b) => a.areanm.localeCompare(b.areanm));
-		places = new MagicArray(...newplaces);
-		place = places[0];
-		lookup = newlookup;
-	}
-	function clickCSV() {
-		csv_upload.click();
-	}
-	function loadCSV() {
-		let file = csv_upload.files[0] ? csv_upload.files[0] : null;
-		
-		if (file) {
-			var reader = new FileReader();
-			reader.onload = function(e) {
-				makeData(e.target.result);
-			};
-			reader.readAsText(file);
-		}
+		[fileHandle] = await window.showOpenFilePicker(options);
+		const file = await fileHandle.getFile();
+		currentCsvName = fileHandle.name;
+		lastModifiedCsv = file.lastModifiedDate.toGMTString();
+		const content = await file.text();
+		makeData(content);
 	}
 
-	function savePUG() {
-		let blob = new Blob([template], { type: "text/pug;charset=utf-8" });
-		download(blob, "template.pug");
+	async function writeNewPUG() {
+		const options = {
+			types: [
+				{
+					description: "PUG file",
+					accept: {
+						"text/plain": [".pug"],
+					},
+				},
+			],
+		};
+
+		const handle = await window.showSaveFilePicker(options);
+		const writable = await handle.createWritable();
+		await writable.write(template);
+		await writable.close();
+		return handle;
 	}
 
 	async function ddjDemo() {
@@ -138,20 +183,6 @@
 		getPUG("./data/nlgtemplate.txt");
 	}
 
-	async function getCSV(url) {
-		console.log("loading csv");
-		let csv_res = await fetch(url);
-		let csv_str = await csv_res.text();
-		makeData(csv_str);
-	}
-
-	async function getPUG(url) {
-		console.log("loading pug");
-		let pug_res = await fetch(url);
-		let pug = await pug_res.text();
-		await sleep(50);
-		editor.setContent(pug);
-	}
 
 	onMount(() => {
 		window.ddjDemo = ddjDemo;
@@ -166,74 +197,153 @@
 			}
 		}
 	});
+
+	async function validatePUG() {
+		progress = 0;
+		let invalid = [];
+
+		let plcs = places ? [null, ...places] : [null];
+
+		for (let i = 0; i < plcs.length; i++) {
+			progress = i == plcs.length - 1 ? 0 : (i + 1) / plcs.length;
+			await sleep(0);
+			let output = render(template, plcs[i], plaintext);
+			if (output == "")
+				invalid.push(plcs[i] ? plcs[i].areanm : "No area selected");
+		}
+
+		if (invalid[0]) {
+			alert(`Validation failed for ${invalid.join(", ")}`);
+		} else {
+			alert("Validation passed for all areas.");
+		}
+	}
+
+	function makeData(str) {
+		let newplaces = csvParse(str, autoType);
+		let newlookup = {};
+		newplaces.forEach((d) => (newlookup[d.areacd] = d));
+		newplaces = newplaces
+			.filter((d) =>
+				["E06", "E07", "E08", "E09", "W06"].includes(
+					d.areacd.slice(0, 3)
+				)
+			)
+			.sort((a, b) => a.areanm.localeCompare(b.areanm));
+		places = new MagicArray(...newplaces);
+		place = places[0];
+		lookup = newlookup;
+	}
+
+	async function getCSV(url) {
+		let csv_res = await fetch(url);
+		let csv_str = await csv_res.text();
+		makeData(csv_str);
+	}
+
+	async function getPUG(url) {
+		let pug_res = await fetch(url);
+		let pug = await pug_res.text();
+		await sleep(50);
+		editor.setContent(pug);
+	}
+
 </script>
 
 <svelte:head>
-	<script src="https://unpkg.com/rosaenlg@3.2.4/dist/rollup/rosaenlg_tiny_en_US_3.2.4_comp.js" on:load={() => rosae = window.rosaenlg_en_US}></script>
+	<script
+		src="https://unpkg.com/rosaenlg@3.2.4/dist/rollup/rosaenlg_tiny_en_US_3.2.4_comp.js"
+		on:load={() => (rosae = window.rosaenlg_en_US)}/>
 	{#if plaintext}
-	<style>
-		mark {
-			background-color: rgba(255,255,255,0);
-		}
-		section > section {
-			margin-left: none;
-		}
-	</style>
+		<style>
+			mark {
+				background-color: rgba(255, 255, 255, 0);
+			}
+			section > section {
+				margin-left: none;
+			}
+		</style>
 	{:else}
-	<style>
-		mark {
-			font-weight: bold;
-			padding: 0 4px;
-			background-color: lightgrey;
-		}
-		prop {
-			background-color: lightgrey;
-		}
-		section {
-			border-top: 1px solid black;
-		}
-		section > section {
-			margin-left: 20px;
-		}
-	</style>
+		<style>
+			mark {
+				font-weight: bold;
+				padding: 0 4px;
+				background-color: lightgrey;
+			}
+			prop {
+				background-color: lightgrey;
+			}
+			section {
+				border-top: 1px solid black;
+			}
+			section > section {
+				margin-left: 20px;
+			}
+		</style>
 	{/if}
 </svelte:head>
 
 <main>
 	<nav>
 		<div>
-			<button on:click={clickPUG}><Icon type="load" margin/> Load PUG file</button>
-			<button on:click={validatePUG}>{#if progress}{(progress * 100).toFixed(0)}%{:else}<Icon type="validate" margin/>{/if} Validate PUG</button>
-			<button on:click={savePUG}><Icon type="save" margin/> Save PUG file</button>
-			<input type="file" accept=".pug" style="display:none" bind:this={pug_upload} on:change={loadPUG}>
-			<input type="file" accept=".csv" style="display:none" bind:this={csv_upload} on:change={loadCSV}>
+			<button on:click={openPUG}
+				><Icon type="load" margin /> Load PUG file</button
+			>
+			<button on:click={validatePUG}
+				>{#if progress}{(progress * 100).toFixed(0)}%{:else}<Icon
+						type="validate"
+						margin
+					/>{/if} Validate PUG</button
+			>
+			<button on:click={writeNewPUG}
+				><Icon type="save" margin /> Save PUG file</button
+			>
 		</div>
 		<div>
-			<button on:click={clickCSV}><Icon type="load" margin/> Load CSV file</button>
+			<button on:click={openCSV}
+				><Icon type="load" margin /> Load CSV file</button
+			>
 			<select bind:value={place} disabled={!places}>
 				{#if places}
-				<option value={null}>No area selected</option>
-				{#each places as option}
-				<option value={option}>{option.areanm}</option>
-				{/each}
+					<option value={null}>No area selected</option>
+					{#each places as option}
+						<option value={option}>{option.areanm}</option>
+					{/each}
 				{:else}
-				<option value={null}>Load a CSV data file</option>
+					<option value={null}>Load a CSV data file</option>
 				{/if}
 			</select>
 			<label>
-				<input type="checkbox" bind:checked={plaintext}/>
+				<input type="checkbox" bind:checked={plaintext} />
 				Plain text
 			</label>
 		</div>
 	</nav>
 	<div class="content">
 		<div>
-			<Editor bind:content={template} bind:this={editor}/>
+			<Editor bind:content={template} bind:this={editor} />
 		</div>
+
 		<div class="preview">
-			{#if rosae && template}
-			{@html render(template, place, plaintext)}
-			{/if}
+			<p class="fixed">
+				{#if rosae && template}
+					{#if currentPugName && !plaintext}
+						<span
+							>You are editing <b>{currentPugName}</b><i>
+								last modified on {lastModifiedPug}</i
+							></span
+						>
+					{/if}
+					{#if currentCsvName && !plaintext}
+						<span>
+							with data from <b>{currentCsvName}</b><i>
+								last modified on {lastModifiedCsv}</i
+							></span
+						>
+					{/if}
+					{@html render(template, place, plaintext)}
+				{/if}
+			</p>
 		</div>
 	</div>
 </main>
@@ -259,14 +369,14 @@
 	}
 	:global(prop.highlighted) {
 		display: inline-block;
-    	vertical-align: top;
+		vertical-align: top;
 		white-space: nowrap;
-  		max-width: 300px;
-  		overflow: hidden;
-  		text-overflow: ellipsis;
+		max-width: 300px;
+		overflow: hidden;
+		text-overflow: ellipsis;
 	}
 	:global(prop.highlighted:hover) {
-  		overflow: visible;
+		overflow: visible;
 	}
 	:global(.text-big) {
 		font-size: 1.3em;
@@ -305,7 +415,7 @@
 		display: flex;
 		flex-direction: row;
 		align-items: center;
-  		flex: 1;
+		flex: 1;
 	}
 	button {
 		display: inline-flex;
@@ -323,7 +433,7 @@
 		overflow: hidden;
 	}
 	.content > div {
-  		flex: 1;
+		flex: 1;
 		overflow-y: auto;
 		height: 100%;
 	}
@@ -333,7 +443,7 @@
 	nav label {
 		margin-left: 12px;
 	}
-	nav input[type=checkbox] {
+	nav input[type="checkbox"] {
 		transform: scale(1.5);
 		filter: saturate(0) contrast(2.5);
 		margin-right: 2.5px;
